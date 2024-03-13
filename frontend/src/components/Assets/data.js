88101b913a7bc9b32b272f7af86cd416d1aeb3b6
farhathulla <farhathulla01@gmail.com>
2024-03-13 15:22:43 +0530

    assets

---
+++ frontend/src/components/Assets/data.js
@@ -0,0 +1,40 @@
+import img1 from './tajmahal.jpg';
+import img2 from './pexels-pixabay-290386.jpg';
+import img3 from './tajgate.jpg';
+import img4 from './head.jpg';
+import img5 from './pexels-pixabay-218480.jpg';
+
+let cover = [
+    {
+        id : 1,
+        image : img1,
+        head : "Taj Mahal",
+        para : "The exqusite white marble"
+    },
+    {
+        id : 2,
+        image : img2,
+        head : "qutub minar",
+        para : "The tallest standing"
+    },
+    {
+        id : 3,
+        image : img3,
+        head : "Church",
+        para : "The cross"
+    },
+    {
+        id : 4,
+        image : img4,
+        head : "Taj Mahal",
+        para : "The exqusite white marble"
+    },
+    {
+        id : 5,
+        image : img5,
+        head : "Taj Mahal",
+        para : "The exqusite white marble"
+    },
+];
+
+export default cover;
