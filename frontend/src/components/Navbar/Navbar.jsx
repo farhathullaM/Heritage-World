8b5efc282f43f7a8d53f89a6b9cfc732f4c373b6
farhathulla <farhathulla01@gmail.com>
2024-03-13 13:19:04 +0530

    navbar section for user pages

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import './Navbar.css';
+import logo_new from '../Assets/image.png';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+
+    let isNavOpen = false;
+function toggleNav(){
+    const navMenu = document.querySelector(".nav-menu")
+    const closeButton = document.querySelector(".menu-icons .fa-times")
+    const menuButton = document.querySelector(".menu-icons .fa-bars")
+    if(isNavOpen){
+        
+        navMenu.classList.remove("nav-open")
+        isNavOpen = false
+        closeButton.style.display = "none"
+        menuButton.style.display = "block"
+    } 
+    else{
+        
+        navMenu.classList.add("nav-open")
+        isNavOpen = true
+        closeButton.style.display = "block"
+        menuButton.style.display = "none"
+    } 
+}
+
+return (
+    <nav className='navbar'>
+        
+        <div className="nav-logo">
+            <img src={logo_new} alt="" />
+            <h1>MONUMENTS</h1>
+        </div>
+
+        <div className="menu-icons" onClick={toggleNav}>
+            <i className='fa-solid fa-bars fa-lg' style={{color: 'white'}}></i>
+            <i className='fa-solid fa-times fa-lg' style={{color: 'white'}}></i>
+        </div>
+
+        <ul className='nav-menu'>
+            <li> <Link to='/' className='navlink'> HOME </Link> </li>
+            
+            <li> <Link to='/about' className='navlink'>  ABOUT US </Link> </li>
+            <li> <Link to='/' className='navlink'>  BLOGS </Link> </li>
+            <li> <Link to='/admin/login' className='navlink'> LOGIN </Link> </li>
+        </ul>
+
+    </nav>
+)
+}
+
+export default Navbar;
