f6bc2d4749ac089d62f611725b2496ab21e323b7
farhathulla <farhathulla01@gmail.com>
2024-03-19 23:03:11 +0530

    hover div action

--- frontend/src/components/LastSection/LastSection.jsx
+++ frontend/src/components/LastSection/LastSection.jsx
@@ -1,54 +1,69 @@
-import React from 'react';
-import one from '../Assets/pexels-pixabay-290386.jpg';
-import two from '../Assets/tajgate.jpg';
-import three from '../Assets/tajmahal.jpg';
-import './LastSection.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import one from "../Assets/pexels-pixabay-290386.jpg";
+import two from "../Assets/tajgate.jpg";
+import three from "../Assets/tajmahal.jpg";
+import "./LastSection.css";
+import { Link } from "react-router-dom";
 
 const LastSection = () => {
-  
   return (
-    <div className='last-section'>
-        <div className="page-head">
-            <h3>Newly Addedd Places</h3>
-        </div>
-        <div className="image-container">
-
-          <div className="img-one-div">
-            <img className='img-one' src={one} alt="" />
-            <div className="details">
+    <div className="last-section">
+      <div className="page-head">
+        <h3>Newly Addedd Places</h3>
+      </div>
+      <div className="image-container">
+        <div className="image-con">
+          <img className="img-one" src={one} alt="" />
+          <div className="details">
             <div className="line"></div>
-              <h2>Miskhal Masjid</h2>
-              <p>Mishkal Mosque is a medieval mosque located in Calicut on Malabar Coast, souther India. The mosque, one of the few surviving medeival mosques in Keerala, is regarded as an important cultural, historical and archtectural monument of Kerala</p>
-            </div>
+            <h2>Miskhal Masjid</h2>
+            <p>
+              Mishkal Mosque is a medieval mosque located in Calicut on Malabar
+              Coast, souther India. The mosque, one of the few surviving
+              medeival mosques in Keerala, is regarded as an important cultural,
+              historical and archtectural monument of Kerala
+            </p>
           </div>
-
-          <div className="img-two-div">
-            <img className='img-two'src={two} alt="" />
-            <div className="details">
-              <div className="line"></div>
-              <h2>Miskhal Masjid</h2>
-              <p>Mishkal Mosque is a medieval mosque located in Calicut on Malabar Coast, souther India. The mosque, one of the few surviving medeival mosques in Keerala, is regarded as an important cultural, historical and archtectural monument of Kerala</p>
-            </div>
-          </div>
-
-          <div className="img-three-div">
-            <img className='img-three'src={three} alt="" />
-            <div className="details">
-              <div className="line"></div>
-              <h2>Miskhal Masjid</h2>
-              <p>Mishkal Mosque is a medieval mosque located in Calicut on Malabar Coast, souther India. The mosque, one of the few surviving medeival mosques in Keerala, is regarded as an important cultural, historical and archtectural monument of Kerala</p>
-            </div>
-          </div>            
-            
         </div>
-        <div className="more-btn-div">
-        <Link to='/explore' style={{textDecoration: 'none'}}>
-          <button className='more-btn'>More <span className="material-symbols-outlined">arrow_forward</span></button>
+
+        <div className="image-con">
+          <img className="img-two" src={two} alt="" />
+          <div className="details">
+            <div className="line"></div>
+            <h2>Miskhal Masjid</h2>
+            <p>
+              Mishkal Mosque is a medieval mosque located in Calicut on Malabar
+              Coast, souther India. The mosque, one of the few surviving
+              medeival mosques in Keerala, is regarded as an important cultural,
+              historical and archtectural monument of Kerala
+            </p>
+          </div>
+        </div>
+
+        <div className="image-con">
+          <img className="img-three" src={three} alt="" />
+          <div className="details">
+            <div className="line"></div>
+            <h2>Miskhal Masjid</h2>
+            <p>
+              Mishkal Mosque is a medieval mosque located in Calicut on Malabar
+              Coast, souther India. The mosque, one of the few surviving
+              medeival mosques in Keerala, is regarded as an important cultural,
+              historical and archtectural monument of Kerala
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="more-btn-div">
+        <Link to="/explore" style={{ textDecoration: "none" }}>
+          <button className="more-btn">
+            More{" "}
+            <span className="material-symbols-outlined">arrow_forward</span>
+          </button>
         </Link>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default LastSection;
