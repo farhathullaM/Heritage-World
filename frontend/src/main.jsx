0bd8cda6a05ea1efdbdd950360f0183bed9987ab
farhathulla <farhathulla01@gmail.com>
2024-03-22 17:44:04 +0530

    Pop up box component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001/";
-// axios.defaults.baseURL = "https://heritage-world.onrender.com";
+// axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "https://heritage-world.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
