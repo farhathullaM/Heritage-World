4003005010d59b61d2891eefdb8fe68cd841c9fc
farhathulla <farhathulla01@gmail.com>
2024-04-01 12:51:22 +0530

    given images added

--- frontend/src/components/Assets/data.js
+++ frontend/src/components/Assets/data.js
@@ -1,40 +1,27 @@
-import img1 from './tajmahal.jpg';
-import img2 from './pexels-pixabay-290386.jpg';
-import img3 from './tajgate.jpg';
-import img4 from './head.jpg';
-import img5 from './pexels-pixabay-218480.jpg';
+import img1 from './rosary_church.jpg';
+import img2 from './jumaath_mosque.jpg';
+import img3 from './maniyoor_temple.png';
+
 
 let cover = [
     {
         id : 1,
         image : img1,
         head : "Holy Rosary Church - Thalassery",
-        para : "Journey with a great sight a wonderful expereince"
+        para : "Built in the memory of the queen of the Holy Rosary"
     },
     {
         id : 2,
         image : img2,
         head : "Jumaath Mosque - Thalassery",
-        para : "Statue represeting unity and freedom"
+        para : "Oldest surviving mosque in Thalasseri buit by Malik Dinar"
     },
     {
         id : 3,
         image : img3,
-        head : "Maliyekkal House - Thalassery",
-        para : "The exquisite white marble brilliance"
-    },
-    {
-        id : 4,
-        image : img4,
-        head : "St. Angelo Fort - Kannur",
-        para : "Brilliance of middle age architecture"
-    },
-    {
-        id : 5,
-        image : img5,
-        head : "Kapparatti Bhavanam - Ponniyam",
-        para : "Church in the light of day"
-    },
+        head : "Maniyoor Sree Subrahmanya Swamy Temple",
+        para : "One of the oldest temples in North Malabar dedicated to lord Subrahmanya"
+    }
 ];
 
 export default cover;
--- frontend/src/components/SecondSection/SecondSection.jsx
+++ frontend/src/components/SecondSection/SecondSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./SecondSection.css";
-import img1 from "../Assets/pexels-pixabay-208277.jpg";
-import img2 from "../Assets/pexels-pixabay-218480.jpg";
+import img1 from "../Assets/st_angelo_fort.png";
+import img2 from "../Assets/kapparatti_bhavanam.jpg";
 import { Link } from "react-router-dom";
 import arrow from "../Assets/right-arrow.png";
 
@@ -14,15 +14,22 @@ const SecondSection = () => {
     <div className="second-section">
       <div className="section-1">
         <div className="image-sec">
-          <img className="sec-img" src={img2} alt="" />
+          <img className="sec-img" src={img1} alt="" />
         </div>
         <div className="details-sec">
-          <h2>Miskhal Masjid</h2>
+          <h2>St.Angelo Fort</h2>
           <p>
-            Mishkal Mosque is a medieval mosque located in Calicut on Malabar
-            Coast, souther India. The mosque, one of the few surviving medeival
-            mosques in Keerala, is regarded as an important cultural, historical
-            and archtectural monument of Kerala
+            The first Portuguese Viceroy in India Don Francesco de Almeida
+            constructed this massive triangular Laterite structure. In 1663 the
+            Dutch captured the fort from the Portuguese and reinforced its
+            fortifications. It was later sold to a native king, Ali raja of
+            Arakkal, in the year 1772. In 1790, the British took control of the
+            fort, renovated it and used it as their chief military base in
+            Malabar till 1947.Features: Main architectural features are gigantic
+            bastions, fortification walls, watch tower, a chapel, laterite stair
+            ways, remains of an early oil lamp light house, army barracks,
+            underground stone path ways, cannons etc. Now the fort is protected
+            by the Archaeological Survey of India.
           </p>
           <Link
             to="/places/65f5c8c8de4a16e914e2f889"
@@ -38,12 +45,14 @@ const SecondSection = () => {
 
       <div className="section-2">
         <div className="details-sec">
-          <h2>Miskhal Masjid</h2>
+          <h2>Kapparatti Bhavanam</h2>
           <p>
-            Mishkal Mosque is a medieval mosque located in Calicut on Malabar
-            Coast, souther India. The mosque, one of the few surviving medeival
-            mosques in Keerala, is regarded as an important cultural, historical
-            and archtectural monument of Kerala
+            Built by the noted Malayalam writer and the author of the first
+            Malayalam short story, Vengayil Kunjuraman Nayanar. The house is
+            also known as Vengayil Madam. An architecture marvel with more than
+            120 rooms. Ample use of wood for the construction made it an
+            excellent example of Kerala tharavads.Now functioning as a Christian
+            seminary.
           </p>
           <Link
             to="/places/65f5ca98de4a16e914e2f89e"
@@ -56,7 +65,7 @@ const SecondSection = () => {
           </Link>
         </div>
         <div className="image-sec">
-          <img className="sec-img" src={img1} alt="" />
+          <img className="sec-img" src={img2} alt="" />
         </div>
       </div>
     </div>
