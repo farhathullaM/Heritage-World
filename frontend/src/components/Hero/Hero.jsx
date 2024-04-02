6991e3c03dab6a7f037028e147d146a66d1f7451
farhathulla <farhathulla01@gmail.com>
2024-04-02 22:45:56 +0530

    before img color

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Hero.css";
 import data from "../Assets/data.js";
-import arrow from '../Assets/right-arrow.png'
+import arrow from "../Assets/right-arrow.png";
 
 const Hero = () => {
   const settings = {
@@ -23,30 +23,31 @@ const Hero = () => {
 
   return (
     <div className="hero">
+ 
       <div className="image-slider">
         <Slider {...settings}>
           {data.map((item, index) => (
             <div key={index}>
-              <img src={item.image} alt={`Slide ${index}`} />
-
-              <div className="black"></div>
-
+              
               <div className="hero-heading">
                 <h1>{item.head}</h1>
                 <p>{item.para}</p>
               </div>
 
+              <div className="black"></div>
+              
+              <img src={item.image} alt={`Slide ${index}`} />
             </div>
           ))}
         </Slider>
       </div>
       <div className="discover">
-      <Link to="/explore" style={{ textDecoration: "none" }}>
-                  <button className="discover-btn">
-                    DISCOVER
-                    <img src={arrow} alt="" />
-                  </button>
-                </Link>
+        <Link to="/explore" style={{ textDecoration: "none" }}>
+          <button className="discover-btn">
+            DISCOVER
+            <img src={arrow} alt="" />
+          </button>
+        </Link>
       </div>
     </div>
   );
