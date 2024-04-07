4230d54cbb7ea81eddeb112c668dc14ab602a126
ishaque risan <riswanp747970@gmail.com>
2024-04-07 11:32:24 +0530

    video and image compressed and base6 4 convert upload .but the image is not show frontend

--- frontend/src/components/addGallery.jsx
+++ frontend/src/components/addGallery.jsx
@@ -69,8 +69,10 @@ const AddGallery = () => {
         <div className="head">Add Gallery </div>
         <form onSubmit={submit}>
           <div className="inp">
-            <label htmlFor="imgTitle" className="required">Title</label>
-            <input name="imgTitle" type="text" id="imgTitle" required/>
+            <label htmlFor="imgTitle" className="required">
+              Title
+            </label>
+            <input name="imgTitle" type="text" id="imgTitle" required />
           </div>
 
           {/* <div className="inp">
@@ -83,7 +85,9 @@ const AddGallery = () => {
             <input name="image" type="file" id="image" ref={imageRef} />
           </div> */}
           <div className="inp">
-            <label htmlFor="image" className="required">Image/Video</label>{" "}
+            <label htmlFor="image" className="required">
+              Image/Video
+            </label>{" "}
             <div className="fileSelect">
               <div className="filebtncon">
                 <label htmlFor="image" className="fileopen btn">
